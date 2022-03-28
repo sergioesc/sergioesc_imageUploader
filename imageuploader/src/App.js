1f2fcbcf8f177adcda3eb9e805671fd1768732e9
Sergio Escobar <93834310+sergioesc@users.noreply.github.com>
2022-03-28 11:06:19 -0400

    Created frontEnd

--- imageuploader/src/App.js
+++ imageuploader/src/App.js
@@ -1,7 +1,18 @@
-import "./App.css";
+import Loading from "./components/Loading.js";
 
 function App() {
-  return <div className="App"></div>;
+  return <div className="App">
+    <div className="upload-container">
+      <h2>Upload your image</h2>
+      <small id="small-text">File should be Jpeg, Png...</small>
+
+      <form className="form-upload">
+        <label>Drag & Drop your image here</label>
+        <input className="input-image" type="file" accept="image/png, image/jpeg"></input>
+      </form>
+    </div>
+    <Loading />
+  </div>;
 }
 
 export default App;
