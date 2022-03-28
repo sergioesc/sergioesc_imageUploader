f4d085c8c07071e3495a288195ef7f0e77cfc0a5
Sergio Escobar <93834310+sergioesc@users.noreply.github.com>
2022-03-28 12:46:10 -0400

    uploadRouter created

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,14 @@
 import express from "express";
+import uploadRouter from "./uploadRouter.js";
+import dotenv from "dotenv";
+dotenv.config();
 
 const app = express();
 
 app.get("/", (req, res) => {
   res.send("home del server");
 });
+app.use("/api/upload", uploadRouter);
 
 const port = process.env.PORT || 5000;
 app.listen(port, function () {
